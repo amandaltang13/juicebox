95f221327027b79910f9dfa2863cd03095b1d0ff
amanda <amandaltang13@gmail.com>
2023-01-16 01:30:34 -0600

    second commit

--- db/index.js
+++ db/index.js
@@ -7,3 +7,32 @@ const client = new Client('postgres://localhost:5432/juicebox-dev');
 module.exports = {
   client,
 }
+
+// inside db/index.js
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+    FROM users;
+  `);
+
+  return rows;
+}
+
+async function createUser({ username, password }) {
+  try {
+    const result = await client.query(`
+    INSERT INTO users (username, password) VALUES ($1, $2);
+    `, ["some_name", "some_password"]);
+
+    return result
+  } catch (error) {
+    throw error;
+  }
+}
+
+// and export them
+module.exports = {
+  client,
+  getAllUsers,
+}
--- db/seed.js
+++ db/seed.js
@@ -5,15 +5,16 @@ const { client } = require('./index');
 
 async function testDB() {
   try {
+    
     // connect the client to the database, finally
     client.connect();
 
     // queries are promises, so we can await them
-    const result = await client.query(`SELECT * FROM users;`);
+    const { rows } = await client.query(`SELECT * FROM users;`);
 
     // for now, logging is a fine way to see what's up
-    console.log(result);
-    console.log("test");
+    console.log(rows);
+    // console.log("test");
   } catch (error) {
     console.error(error);
   } finally {
@@ -21,5 +22,80 @@ async function testDB() {
     client.end();
   }
 }
+
+const {
+  // client,
+  getAllUsers // new
+} = require('./index');
+
+async function testDB() {
+  try {
+    // client.connect();
+    console.log("Starting to test database...");
+
+    const users = await getAllUsers();
+    console.log("getAllUsers:", users);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+// inside db/seed.js
+
+// this function should call a query which drops all tables from our database
+async function dropTables() {
+  try {
+    console.log("Starting to drop tables...");
+    await client.query(`
+    DROP TABLE IF EXISTS users;
+    `);
+
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error; // we pass the error up to the function that calls dropTables
+  }
+}
+
+// this function should call a query which creates all tables for our database 
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username varchar(255) UNIQUE NOT NULL,
+      password varchar(255) NOT NULL
+    );
+    `);
+
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error('Error building tables!');
+    throw error; // we pass the error up to the function that calls createTables
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    throw error;
+    // console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+rebuildDB()
+.then(testDB)
+.catch(console.error)
+.finally(() => client.end());
 
 testDB();
